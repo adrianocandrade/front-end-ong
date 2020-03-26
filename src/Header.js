597cf6815224a945866b1282dd94f2e6ea6e2f81
adrianocandrade <adrianowebm@gmail.com>
2020-03-26 13:51:21 -0300

    Commit Inicial

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Help to Heros</h1>
+        </header>
+    );
+}
---
+++ src/pages/Register/index.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
+import api from '../../services/api';
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function Logon() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [facebook, setFacebook] = useState('');
+    const [instagram, setInstagram] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+    const [site, setSite] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+        const data = {
+            name,
+            email,
+            city,
+            uf,
+            whatsapp,
+            facebook,
+            instagram,
+            site,
+        }
+
+        try {
+            const response = await api.post('ongs', data);
+
+            alert(`Seu id de acesso ${response.data.id}`);
+            history.push('/');
+        } catch (err) {
+            alert(`Erro no cadastro, tente novamente`)
+        }
+    }
+
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Help the Heroes" />
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
+
+                    <Link to="/" className="black-link">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Voltar para login
+                    </Link>
+                </section>
+
+                <form onSubmit={handleRegister}>
+                    <input placeholder="Nome da ONG" value={name} onChange={e => setName(e.target.value)} />
+                    <input type="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} />
+                    <div className="input-group">
+                        <input placeholder="whatsApp" value={whatsapp} onChange={e => setWhatsapp(e.target.value)} />
+                        <input placeholder="facebook" value={facebook} onChange={e => setFacebook(e.target.value)} />
+                        <input placeholder="instagram" value={instagram} onChange={e => setInstagram(e.target.value)} />
+                    </div>
+
+                    <div className="input-group">
+                        <input placeholder="Cidade" value={city} onChange={e => setCity(e.target.value)} />
+                        <input placeholder="UF" style={{ width: 80 }} value={uf} onChange={e => setUf(e.target.value)} />
+                    </div>
+
+                    <input placeholder="Seu site" value={site} onChange={e => setSite(e.target.value)} />
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+
+            </div>
+        </div>
+    );
+}
